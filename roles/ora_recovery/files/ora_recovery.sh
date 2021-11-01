20959aa46606cfba1de9efa553e2d180b36d6727
Ahmad, Sufian <Sufian.Ahmad@netapp.com>
2021-11-01 12:52:24 -0500

    ora_recovery.sh edited online with Bitbucket

--- roles/ora_recovery/files/ora_recovery.sh
+++ roles/ora_recovery/files/ora_recovery.sh
@@ -1,7 +1,7 @@
 #/usr/bin/sh
 # Description: Restore Oracle databases to specified recovery point: a scn, or a time stamp after a snapshot restore.
 # The script search Oracle databases in /etc/oratab file in "running mode or Y" and
-# loop through each database on target server to complete an incomplete recovery and reset log file to open the databases. 
+# loop through each database on target server to complete an incomplete recovery and reset log file to open the databases.
 
 ORATAB=/etc/oratab
 if [ ! -f $ORATAB ]
@@ -20,25 +20,28 @@ case "$1" in
      export ORACLE_SID
      export ORACLE_HOME
      export PATH=$PATH:$ORACLE_HOME/bin
-     export SCRIPT_DIR=/tmp
-     $ORACLE_HOME/bin/sqlplus -s "/ as sysdba" <<EOF>> $1_$log
+     $ORACLE_HOME/bin/sqlplus -s "/ as sysdba" <<!>> db_mount
      whenever sqlerror exit SQL.SQLCODE
      shutdown abort;
      startup mount;
+     exit;
+!
+     export last_scn=`$ORACLE_HOME/bin/sqlplus -s "/ as sysdba" <<!!
+     whenever sqlerror exit SQL.SQLCODE
      set pagesize 0
      set head off
      set feed off
      set echo off
      set verify off
-     spool $SCRIPT_DIR/$ORACLE_SID.last_scn
-     select max(next_change#)-1 from v\$archived_log;
-     spool off
-     !sleep 5
-     !export LAST_SCN=`cat $SCRIPT_DIR/$ORACLE_SID.last_scn | tr -d ' '`    
-     recover automatic database until change $LAST_SCN using backup controlfile;
+     select max(next_change#)-1 from v\\$archived_log;
+     exit;
+!!`
+     $ORACLE_HOME/bin/sqlplus -s "/ as sysdba" <<!!!>> $1_$log
+     whenever sqlerror exit SQL.SQLCODE
+     recover automatic database until change $last_scn using backup controlfile;
      alter database open resetlogs;
      exit;
-EOF
+!!!
    done
    ;;
 
